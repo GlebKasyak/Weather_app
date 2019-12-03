66d9eaf558d285acc5b4e9ccfccc84d2d0f30a97
gleb <gkebkasyak@gmail.com>
2019-12-03 23:35:17 +0300

    Updates

---
+++ precache-manifest.f7be4057c4002f7985edfb49efa0cc3f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92787c364d47a3f65d920a09dbc657ee",
+    "url": "/Weather_app/index.html"
+  },
+  {
+    "revision": "0f5604a4feff0ff0a387",
+    "url": "/Weather_app/static/css/main.8bc15804.chunk.css"
+  },
+  {
+    "revision": "ca85c28d80e226b11752",
+    "url": "/Weather_app/static/js/2.ab089c0f.chunk.js"
+  },
+  {
+    "revision": "0f5604a4feff0ff0a387",
+    "url": "/Weather_app/static/js/main.a88a7d63.chunk.js"
+  },
+  {
+    "revision": "4c71f5271cb2cbc75aa8",
+    "url": "/Weather_app/static/js/runtime-main.cd5954a5.js"
+  },
+  {
+    "revision": "1e070d87818041ee71eaa9bcba382622",
+    "url": "/Weather_app/static/media/bottomSection.1e070d87.jpg"
+  }
+]);
